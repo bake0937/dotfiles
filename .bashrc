94ff99f1d722816e5085beec51f3a3188d013fc0
bake0937 <sadayasu.info@gmail.com>
2017-06-17 23:12:40 +0900

    .bashrcを追加

---
+++ .bashrc
@@ -0,0 +1,4 @@
+source /usr/local/etc/bash_completion.d/git-prompt.sh
+source /usr/local/etc/bash_completion.d/git-completion.bash
+GIT_PS1_SHOWDIRTYSTATE=true
+export PS1='\h\[\033[00m\]:\W\[\033[31m\]$(__git_ps1 [%s])\[\033[00m\]\$ '
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile$
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
